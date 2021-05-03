61762c4e376a1841519d9891dcf4e8c241af66a4
a000011 <anton.nadikin@gamil.com>
2021-05-03 19:59:05 +0300

    Улучшил банер

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,9 +5,29 @@
 @section('content')
     <div class="container-fluid" id="indexList">
         <div id="banner" class="banner">
-            <h1>Установка ГБО в Тольятти</h1>
-            <h2>ул. Ларина 145/1<br />89278998878</h2>
-            <div class="logo"></div>
+            <h1 class="bannerh1">Установка ГБО в Тольятти</h1>
+            {{-- <h2>ул. Ларина 145/1<br />89278998878</h2> --}}
+            <ul clas="info d-flex" style="margin: 0 auto;">
+                <li>
+                    <img src="/images/icons/check.png" class="align-middle">
+                    <h1 class="d-inline-block py-auto align-middle hList">
+                        Низкая цена
+                    </h1>
+                </li>
+                <li>
+                    <img src="/images/icons/check.png" class="align-middle">
+                    <h1 class="d-inline-block py-auto align-middle hList">
+                        Гарантия
+                    </h1>
+                </li>
+                <li>
+                    <img src="/images/icons/check.png" class="align-middle">
+                    <h1 class="d-inline-block py-auto align-middle hList">
+                        Качество
+                    </h1>
+                </li>
+            </ul>
+            {{-- <div class="logo"></div> --}}
         </div>
 
         <div class="row" id="servicesRow">
@@ -16,7 +36,7 @@
             </h1>
             <div id="services" align="center" class="row col-md-10 text-center"></div>
         </div>
-        <div class="row" id="exampleRow">
+        <div class="row">
 
         </div>
     </div>
