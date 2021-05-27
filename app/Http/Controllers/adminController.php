a7bf188fd4a3bc9f2479cb492b853a4d0434169a
a000011 <anton.nadikin@gamil.com>
2021-05-27 12:47:07 +0300

    add validation

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -10,6 +10,7 @@ use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\File;
 use Illuminate\Support\Facades\Storage;
 use App\Models\examples;
+use Illuminate\Support\Facades\Validator;
 
 class adminController extends Controller {
 	function auth(Request $request) {
@@ -26,22 +27,39 @@ class adminController extends Controller {
 	}
 
 	function addExample(Request $request) {
-		// $path = $request->file('photos')->store('TEST123');
-		$images = $request->file('photos');
-		$num = 0;
-		$path = '';
-		foreach ($images as $image) {
-			$path =
-				$path .
-				'storage/images/' .
-				$image->storeAs('public', ++$num . '.' . $image->extension()) .
-				';';
+		$validator = Validator::make($request->all(), [
+			'car_name' => 'required|Min:4',
+			'price' => 'required|numeric',
+		]);
+		if ($validator->fails()) {
+			$errors = $validator->errors();
+			return view('adminPanel', [
+				'errors' => $errors,
+				'translate' => [
+					'car_name' => 'Название',
+					'price' => 'цена',
+				],
+			]);
+		} else {
+			$images = $request->file('photos');
+			$num = 0;
+			$path = '';
+			foreach ($images as $image) {
+				$path =
+					$path .
+					'storage/images/' .
+					$image->storeAs(
+						'public',
+						++$num . '.' . $image->extension(),
+					) .
+					';';
+			}
+			$example = new examples();
+			$example->car_name = $request->input('car_name');
+			$example->price = $request->input('price');
+			$example->img = $path;
+			$example->save();
+			return view('adminPanel');
 		}
-		$example = new examples();
-		$example->car_name = $request->input('car_name');
-		$example->img = $path;
-		$example->price = $request->input('price');
-		$example->save();
-		return view('adminPanel');
 	}
 }
--- resources/views/adminPanel.blade.php
+++ resources/views/adminPanel.blade.php
@@ -15,26 +15,45 @@
 
 <body class="bg-dark">
     <h1 align="center">Панель администратора</h1>
-    <form method="post" action="" enctype="multipart/form-data" class="col-4 offset-lg-3">
-        {{ csrf_field() }}
+    <center>
+        <h3 style="background-color:rgb(143, 14, 14);">
+            @if (isset($errors))
+                @foreach ( $errors->messages() as $key => $messages)
+                        @foreach ($translate as $trkey => $tr)
+                            @if ($trkey == $key)
+                                {{ $tr }}
+                            @endif
+                        @endforeach
+                        :
+                        @foreach ($messages as $message)
+                            {{$message}}<br>
+                        @endforeach
+                @endforeach
+            @endif
+        </h3>
+    </center>
+    <center>
+        <form method="post" action="" enctype="multipart/form-data" class="col-4 ">
+            {{ csrf_field() }}
 
-        <div class="form-group">
-            <label for="name">Название машины</label>
-            <input type="text" name="car_name" class="form-control" id="name" placeholder="Название машины">
-        </div>
+            <div class="form-group">
+                <label for="name">Название машины</label>
+                <input type="text" name="car_name" class="form-control" id="name" placeholder="Название машины">
+            </div>
 
-        <div class="form-group">
-            <label for="price">Цена установки</label>
-            <input type="text" name="price" class="form-control" id="price" placeholder="Цена установки">
-        </div>
+            <div class="form-group">
+                <label for="price">Цена установки</label>
+                <input type="number" name="price" class="form-control" id="price" placeholder="Цена установки">
+            </div>
 
-        <div class="form-group">
-            <label for="file">Загрузите фотографии  </label>
-            <input type="file" name="photos[]" class="form-control-file" multiple id="file">
-        </div>
+            <div class="form-group">
+                <label for="file">Загрузите фотографии  </label>
+                <input type="file" name="photos[]" class="form-control-file" multiple id="file">
+            </div>
 
-        <button type="submit" class="btn btn-primary">Добавить работу</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Добавить работу</button>
+        </form>
+    </center>
 </body>
 </html>
 
